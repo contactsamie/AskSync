c1669b68e75508bb243bf45c5a923600327f4afb
contactsamie <contactsamie@gmail.com>
2017-04-05 00:28:25 -0400

    fix paket build

--- AskSync/build.bat
+++ AskSync/build.bat
@@ -1,6 +1,7 @@
 @echo on
 cls
-paket.bootstrapper.exe
-paket restore --force
+paket auto-restore on
+.paket\paket.bootstrapper.exe
+.paket\paket update --redirects  --createnewbindingfiles
 packages\FAKE\tools\Fake.exe build.fsx
 pause
