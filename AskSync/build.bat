fbd608ba526091f957c9e2f49910d497de6fd6c2
Samuel <sam.bamgboye@officialcommunity.com>
2017-04-05 11:40:10 -0400

    update

--- AskSync/build.bat
+++ AskSync/build.bat
@@ -1,6 +1,6 @@
 @echo on
 cls
-paket auto-restore on
+.\paket auto-restore on
 .paket\paket.bootstrapper.exe
 .paket\paket update --redirects  --createnewbindingfiles
 packages\FAKE\tools\Fake.exe build.fsx
